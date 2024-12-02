c1262e283fc3de1b13da0d2e519feeebb3db876f
Kamanpreet Singh <kskaman@gmail.com>
2024-12-01 21:26:23 -0330

    starting writing sudoku hub in react + js

---
+++ react-javascript-app/src/App.jsx
@@ -0,0 +1,7 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+const App = () => {
+  return <Header message={ "Welcome to Sudoku Hub"} />  
+}
+
+export default App;
---
+++ react-javascript-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(message) {
+    return <header >
+        <h1>
+            {message}
+        </h1>
+    </header>
+}
+
+export default Header;
